0333d6d5833d5d03f9117f27754f2378a814632a
Benji <benjamin.bruckmoser@web.de>
2021-02-11 11:03:03 +0100

    Add Steam test script

---
+++ Assets/Scripts/Runtime/HelloSteam.cs
@@ -0,0 +1,17 @@
+﻿using Steamworks;
+using UnityEngine;
+
+namespace Runtime
+{
+    public class HelloSteam : MonoBehaviour
+    {
+        void Start()
+        {
+            if (SteamManager.Initialized)
+            {
+                string personaName = SteamFriends.GetPersonaName();
+                Debug.Log(personaName);
+            }
+        }
+    }
+}
