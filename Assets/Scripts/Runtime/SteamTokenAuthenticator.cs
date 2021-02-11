3e85a8912ed1fbd3cf60eb3281e6df91e2648594
Benji <benjamin.bruckmoser@web.de>
2021-02-11 21:48:42 +0100

    Refactor steam token authenticator

--- Assets/Scripts/Runtime/SteamTokenAuthenticator.cs
+++ Assets/Scripts/Runtime/SteamTokenAuthenticator.cs
@@ -1,223 +1,214 @@
 ﻿using System;
 using System.Collections;
-using System.Collections.Generic;
 using System.IO;
 using Mirror;
-using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
-#if !UNITY_SERVER
-using Steamworks;
-#endif
 using UnityEngine;
 using UnityEngine.Networking;
 
-public class SteamTokenAuthenticator : NetworkAuthenticator
+namespace Runtime
 {
-    public static string AuthTicket;
-
-    private string _webAPIKey;
-    private string _appID;
-
-    private const int OK_RESPONSE_CODE = 200;
-
-    private struct AuthRequestMessage : NetworkMessage
+    public class SteamTokenAuthenticator : NetworkAuthenticator
     {
-        public string Ticket;
-        public string SteamId;
-    }
+        public static string AuthTicket;
 
-    private struct AuthResponseMessage : NetworkMessage
-    {
-        public string FailReason;
-    }
+        private string _webAPIKey;
+        private string _appID;
 
-    [Serializable]
-    public class SteamAuthResponse
-    {
-        public string result;
-        public string steamid;
-        public string ownersteamid;
-        public bool vacbanned;
-        public bool publisherbanned;
-    }
+        private const int OkResponseCode = 200;
+        private const string WebApiKeyPath = "data/web_api_key.txt";
+        private const string SteamAppIdPath = "data/steam_appid.txt";
+        private const string SteamApiUserAuthUri = "https://partner.steam-api.com/ISteamUserAuth/AuthenticateUserTicket/v1/";
 
-    #region Server
-
-    public override void OnStartServer()
-    {
-        NetworkServer.RegisterHandler<AuthRequestMessage>(OnAuthRequestMessage, false);
-    }
-
-    public override void OnServerAuthenticate(NetworkConnection conn)
-    {
-    }
-
-    private void OnAuthRequestMessage(NetworkConnection conn, AuthRequestMessage msg)
-    {
-        StartCoroutine(__ValidateToken(conn, msg.Ticket, msg.SteamId));
-    }
-
-    private IEnumerator __ValidateToken(NetworkConnection conn, string ticket, string steamid)
-    {
-        Debug.Log("Server is asked to validate token: " + steamid);
-
-        if (string.IsNullOrEmpty(_webAPIKey))
+        private struct AuthRequestMessage : NetworkMessage
         {
-            string path = "Assets/Server/web_api_key.txt";
+            public string Ticket;
+            public string SteamId;
+        }
+
+        private struct AuthResponseMessage : NetworkMessage
+        {
+            public string FailReason;
+        }
+
+        [Serializable]
+        public class SteamAuthResponse
+        {
+            public string result;
+            public string steamid;
+            public string ownersteamid;
+            public bool vacbanned;
+            public bool publisherbanned;
+        }
+
+        public override void OnServerAuthenticate(NetworkConnection conn)
+        {
+        }
+
+        public override void OnClientAuthenticate(NetworkConnection conn)
+        {
+            AuthRequestMessage authRequestMessage = new AuthRequestMessage()
+            {
+                Ticket = AuthTicket,
+                SteamId = Steamworks.SteamUser.GetSteamID().m_SteamID.ToString()
+            };
+            NetworkClient.Send(authRequestMessage);
+        }
+
+        #region Server
+
+#if UNITY_SERVER || UNITY_EDITOR
+        public override void OnStartServer()
+        {
+            NetworkServer.RegisterHandler<AuthRequestMessage>(OnAuthRequestMessage, false);
+        }
+
+        private void OnAuthRequestMessage(NetworkConnection conn, AuthRequestMessage msg)
+        {
+            StartCoroutine(__ValidateToken(conn, msg.Ticket, msg.SteamId));
+        }
+
+        private IEnumerator __ValidateToken(NetworkConnection conn, string ticket, string steamid)
+        {
+            string parameters;
+            UnityWebRequest webRequest;
+        
+            try
+            {
+                if (string.IsNullOrEmpty(_webAPIKey))
+                {
+                    FetchWebApiToken();
+                }
+
+                if (string.IsNullOrEmpty(_appID))
+                {
+                    FetchAppId();
+                }
+
+                parameters = $"?key={_webAPIKey}&ticket={ticket}&appid={_appID}";
+                webRequest = UnityWebRequest.Get(SteamApiUserAuthUri + parameters);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                ValidateTokenFailed(conn, "Server Exception.");
+                yield break;
+            }
+
+            if (!string.IsNullOrEmpty(parameters) || webRequest == null)
+            {
+                yield break;
+            }
+
+            yield return webRequest.SendWebRequest();
+
+            try
+            {
+                if (webRequest.isNetworkError || webRequest.isHttpError || webRequest.responseCode != OkResponseCode)
+                {
+                    ValidateTokenFailed(conn, "Network or HTTP error occurred.");
+                }
+                else
+                {
+                    HandleTokenValidation(conn, webRequest.downloadHandler.text, ticket, steamid);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                ValidateTokenFailed(conn, "Server Exception.");
+            }
+        }
+
+        private void FetchWebApiToken()
+        {
+            string path = WebApiKeyPath;
             StreamReader reader = new StreamReader(path);
             _webAPIKey = reader.ReadToEnd();
             reader.Close();
         }
 
-        if (string.IsNullOrEmpty(_appID))
+        private void FetchAppId()
         {
-            _appID = "1552150";
+            string path = SteamAppIdPath;
+            StreamReader reader = new StreamReader(path);
+            _appID = reader.ReadToEnd();
+            reader.Close();
         }
 
-        Debug.Log("appid: " + _appID);
-        Debug.Log("key: " + _webAPIKey);
 
-        string parameters = $"?key={_webAPIKey}&ticket={ticket}&appid={_appID}";
-
-        using (UnityWebRequest webRequest = UnityWebRequest.Get("https://partner.steam-api.com/ISteamUserAuth/AuthenticateUserTicket/v1/" + parameters))
+        private void HandleTokenValidation(NetworkConnection conn, string text, string ticket, string steamId)
         {
-            Debug.Log("sent web request");
-            yield return webRequest.SendWebRequest();
-
-
-             
-            if (webRequest.isNetworkError || webRequest.isHttpError)
+            try
             {
-                Debug.Log("error occured: " + webRequest.error);
-                ValidateTokenFailed(conn, "Network or HTTP error occurred.");
+                var jObject = JObject.Parse(text);
+                var jToken = jObject["response"]?["params"];
+
+                if (jToken != null)
+                {
+                    var authResponse = jToken.ToObject<SteamAuthResponse>();
+
+                    if (authResponse != null && (!authResponse.vacbanned || !authResponse.publisherbanned || authResponse.result != "OK" ||
+                                                 authResponse.steamid != steamId))
+                    {
+                        ValidateTokenSucceeded(conn, steamId);
+                        return;
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Debug.Log(e);
+            }
+
+            ValidateTokenFailed(conn, "Authentication failed: " + text);
+        }
+
+        private void ValidateTokenSucceeded(NetworkConnection conn, string steamId)
+        {
+            AuthResponseMessage authResponseMessage = new AuthResponseMessage();
+            conn.Send(authResponseMessage);
+            OnServerAuthenticated.Invoke(conn);
+            //TODO: Add connection to player list with steamId
+        }
+
+        private void ValidateTokenFailed(NetworkConnection conn, string reason)
+        {
+            AuthResponseMessage authResponseMessage =
+                new AuthResponseMessage()
+                {
+                    FailReason = reason
+                };
+            conn.Send(authResponseMessage);
+        }
+#endif
+
+        #endregion
+
+
+        #region Client
+
+#if !UNITY_SERVER
+        public override void OnStartClient()
+        {
+            NetworkClient.RegisterHandler<AuthResponseMessage>(OnAuthResponseMessage, false);
+        }
+
+        private void OnAuthResponseMessage(NetworkConnection conn, AuthResponseMessage msg)
+        {
+            if (string.IsNullOrEmpty(msg.FailReason))
+            {
+                Debug.Log("Authenticator success. Player may connect.");
+                OnClientAuthenticated.Invoke(conn);
             }
             else
             {
-                if (webRequest.responseCode != OK_RESPONSE_CODE)
-                {
-                    ValidateTokenFailed(conn, "Unhandled response code of " + webRequest.responseCode);
-                }
-                else
-                {
-                    if (conn == null)
-                    {
-                        Debug.Log("conn is null");
-                    }
-                    
-                    if (webRequest.downloadHandler == null)
-                    {
-                        Debug.Log("downloadhandler is null");
-                    }
-                    
-                    HandleValidateToken(conn, webRequest.downloadHandler.text, ticket, steamid);
-                }
+                Debug.LogError("Authenticator failed " + msg.FailReason);
+                NetworkClient.Disconnect();
             }
         }
-    }
-
-
-    private void HandleValidateToken(NetworkConnection conn, string text, string ticket, string steamid)
-    {
-        if (conn == null)
-        {
-            Debug.Log("conn is null");
-        }
-
-        if (text == null)
-        {
-            Debug.Log("text is null");
-        }
-
-        if (ticket == null)
-        {
-            Debug.Log("ticket is null");
-        }
-
-        if (steamid == null)
-        {
-            Debug.Log("steamid is null");
-        }
-
-        Debug.Log("Web Request Download Handler: " + text);
-        
-        var jObject = JObject.Parse(text);
-
-        var jToken = jObject["response"]["params"];
-
-        if (jToken != null)
-        {
-            var authResponse = jToken.ToObject<SteamAuthResponse>();
-
-            if (authResponse != null && (!authResponse.vacbanned || !authResponse.publisherbanned || authResponse.result != "OK" ||
-                                         authResponse.steamid != steamid))
-            {
-                ValidateTokenSuccess(conn, ticket, steamid);
-                return;
-            }
-        }
-
-        ValidateTokenFailed(conn, "Authentication failed: " + text);
-    }
-
-    private void ValidateTokenSuccess(NetworkConnection conn, string ticket, string steamid)
-    {
-        Debug.Log("ValidateTokenSuccess. Id of " + steamid);
-        AuthResponseMessage authResponseMessage = new AuthResponseMessage();
-        conn.Send(authResponseMessage);
-        OnServerAuthenticated.Invoke(conn);
-    }
-
-    private void ValidateTokenFailed(NetworkConnection conn, string reason)
-    {
-        AuthResponseMessage authResponseMessage =
-            new AuthResponseMessage()
-            {
-                FailReason = reason
-            };
-        conn.Send(authResponseMessage);
-    }
-
-    #endregion
-
-
-    #region Client
-
-    public override void OnStartClient()
-    {
-        NetworkClient.RegisterHandler<AuthResponseMessage>(OnAuthResponseMessage, false);
-    }
-
-    public override void OnClientAuthenticate(NetworkConnection conn)
-    {
-#if !UNITY_SERVER
-        AuthRequestMessage authRequestMessage = new AuthRequestMessage()
-        {
-            Ticket = AuthTicket,
-            SteamId = SteamUser.GetSteamID().m_SteamID.ToString()
-        };
-
-        NetworkClient.Send(authRequestMessage);
 #endif
-    }
-
-    private void OnAuthResponseMessage(NetworkConnection conn, AuthResponseMessage msg)
-    {
-        if (string.IsNullOrEmpty(msg.FailReason))
-        {
-            Debug.Log("Authenticator success. Player may connect.");
-            OnClientAuthenticated.Invoke(conn);
-        }
-        else
-        {
-            Debug.LogError("Authenticator failed " + msg.FailReason);
-            NetworkClient.Disconnect();
-        }
-    }
-
-    #endregion
-
-
-    public void DeserializeJSONObject()
-    {
+    
+        #endregion
     }
 }
