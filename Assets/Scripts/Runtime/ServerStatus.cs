0147916b210e7101584ee730c2f7a18399a71013
Benji <benjamin.bruckmoser@web.de>
2021-02-11 11:40:27 +0100

    Add JSON.NET, Add ServerStatusService
    ServerStatusService can fetch server status from API

---
+++ Assets/Scripts/Runtime/ServerStatus.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Runtime
+{
+    [Serializable]
+    public class ServerStatus
+    {
+        public string name;
+        public string status;
+        public int maxConnections;
+        public string location;
+        public string ip;
+    }
+}
---
+++ Assets/Scripts/Runtime/ServerStatusService.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using Newtonsoft.Json;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.Networking;
+
+namespace Runtime
+{
+    public class ServerStatusService : MonoBehaviour
+    {
+        public static ServerStatusService Instance { get; private set; }
+
+        public UnityEvent serverStatusReceived;
+        
+        public ServerStatus[] serverStatus { get; private set; }
+        
+
+        private void Awake()
+        {
+            if (Instance == null)
+            {
+                Instance = this;
+                DontDestroyOnLoad(gameObject);
+            }
+            else
+            {
+                Destroy(this);
+            }
+        }
+
+        public void SendGetRequest(string uri)
+        {
+            StartCoroutine(GetRequest(uri));
+        }
+
+        private IEnumerator GetRequest(string uri)
+        {
+            using (UnityWebRequest webRequest = UnityWebRequest.Get(uri))
+            {
+                webRequest.useHttpContinue = false;
+                
+                yield return webRequest.SendWebRequest();
+
+                if (webRequest.isNetworkError)
+                {
+                    Debug.Log("Error: " + webRequest.error);
+                }
+                else
+                {
+                    serverStatus = JsonConvert.DeserializeObject<ServerStatus[]>(webRequest.downloadHandler.text);
+                    serverStatusReceived.Invoke();
+                }
+            }
+        }
+    }
+}
